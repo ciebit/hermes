313127050720a783e063dd498fc837c010736f89
Johnny Saymon <saymon@ciebit.com.br>
2017-10-26 14:45:49 -0300

    Alterado organizacao de modulo

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-# js-Hermes #
+# @ciebit/hermes
 
 Módulo comunicador de eventos.
+
+## Instalação
+
+## Browser
+
+Os módulos Ciebit foram projetos para trabalhar com o [RequireJs](http://requirejs.org/), para usar basta passar como dependência, assim:
+
+```
+#!javascript
+require(["@ciebit/hermes"], function(hermes){
+    // Utilizar aqui
+});
+```
+
+E adicionar nas configurações:
+
+```
+#!javascript
+require.config({
+    paths: {
+        "@ciebit/hermes": "//js.ciebit.com/hermes/v3.0.0.js"
+    }
+});
+```
+
+## Via NPM
+
+Para embutir no projeto ele pode ser baixando através do comando abaixo:
+
+```
+#!shell
+npm install @ciebit/hermes
+```
+
+## Utilização
+
+Você poderá usar o Hermes para armazenar solicitações funções de callback. Exemplo:
+
+```
+#!typescript
+import { Hermes } from "@ciebit/hermes";
+
+let HermesObj:Hermes = new Hermes;
+
+/*
+Aqui é passando como primeiro parâmetro um identificador do evento
+e no segundo uma função de callback
+*/
+HermesObj.aviseMe('ativado', function(){ console.log('Fui ativado') });
+
+
+/*
+A função informada acima será chamada com o comando abaixo
+*/
+HermesObj.avise('ativado');
+```
--- src/Hermes.ts
+++ src/Hermes.ts
@@ -1,101 +1,100 @@
-module Ciebit
+class Hermes
 {
-    export class Hermes
+    private static contador:number = 0;
+    private objId:string;
+    private unicos:Object = {};
+    private solicitacoes:Object = {};
+    private referencias:Object = {};
+
+    public constructor() {
+        this.objId = 'cb-hermes-'+ Hermes.contador++;
+    }
+
+    /*
+    * Método: Atrela uma ação a uma função
+    * @recebe:
+    *  funcao - Função a ser executada quando ocorrer a ação
+    *  acao - Indentifiação da ação
+    *  ref - Referencia da função para exclusão posterior
+    *  unica - Informa se a função deve ser chamada apenas uma vez
+    * @retorna: Nada
+    * @efeito: Nada
+    */
+    public aviseMe( acao:string, funcao:Function, ref:string = undefined, unica:boolean = undefined ):number
     {
-        private static contador:number = 0;
-        private objId:string;
-        private unicos:Object = {};
-        private solicitacoes:Object = {};
-        private referencias:Object = {};
-
-        public constructor() {
-            this.objId = 'cb-hermes-'+ Hermes.contador++;
+        //Criando item caso não exista
+        if( !( acao in this.solicitacoes ) )
+        {
+            this.solicitacoes[ acao ] = [];
+            this.unicos[ acao ] = [];
         }
 
-        /*
-        * Método: Atrela uma ação a uma função
-        * @recebe:
-        *  funcao - Função a ser executada quando ocorrer a ação
-        *  acao - Indentifiação da ação
-        *  ref - Referencia da função para exclusão posterior
-        *  unica - Informa se a função deve ser chamada apenas uma vez
-        * @retorna: Nada
-        * @efeito: Nada
-        */
-        public aviseMe ( acao:string, funcao:Function, ref:string = undefined, unica:boolean = undefined ):number
+        //Adicinando função
+        let chave = this.solicitacoes[ acao ].push( funcao );
+
+        //Adicionando referencia caso haja
+        if( ref ) this.referencias[ ref ] = { 'acao': acao, 'chave': chave-- };
+
+        //Caso deva ser chamado uma única vez
+        if( unica ) this.unicos[ acao ].push( chave );
+
+        return chave;
+    }
+
+    /**
+    * Método: Executar funções que tenham solicitado avisos
+    * ----
+    * @recebe: acao - Indentificação da ação
+    * @retorna: o próprio
+    * @efeito: chama Funções
+    */
+    public avise( acao:string, ...args ):this
+    {
+        // Verifica se existe alguma função para o aviso
+        if( !this.solicitacoes[ acao ] ) return;
+
+        // Percorrendo todas as solicitações
+        for( let chave in this.solicitacoes[ acao ] )
         {
-            //Criando item caso não exista
-            if( !( acao in this.solicitacoes ) )
+            // Pegando os argumentos com excessão do primeiro
+            //var args = Array.prototype.slice.call( arguments, 1 );
+
+            // Chamando a função com os argumentos passados
+            this.solicitacoes[ acao ][ chave ].apply( this, args );
+
+            // Verificando se só deseja uma unica chamada
+            let i = this.unicos[ acao ].indexOf( chave );
+            if( i >= 0 )
             {
-                this.solicitacoes[ acao ] = [];
-                this.unicos[ acao ] = [];
+                // Removendo função para evitar ser
+                // chamada novamente
+                delete this.solicitacoes[ acao ][ chave ];
+                delete this.unicos[ i ];
             }
-
-            //Adicinando função
-            let chave = this.solicitacoes[ acao ].push( funcao );
-
-            //Adicionando referencia caso haja
-            if( ref ) this.referencias[ ref ] = { 'acao': acao, 'chave': chave-- };
-
-            //Caso deva ser chamado uma única vez
-            if( unica ) this.unicos[ acao ].push( chave );
-
-            return chave;
         }
 
-        /**
-        * Método: Executar funções que tenham solicitado avisos
-        * ----
-        * @recebe: acao - Indentificação da ação
-        * @retorna: o próprio
-        * @efeito: chama Funções
-        */
-        public avise( acao:string, ...args ):this
-        {
-            // Verifica se existe alguma função para o aviso
-            if( !this.solicitacoes[ acao ] ) return;
+        return this;
+    }
 
-            // Percorrendo todas as solicitações
-            for( let chave in this.solicitacoes[ acao ] )
-            {
-                // Pegando os argumentos com excessão do primeiro
-                //var args = Array.prototype.slice.call( arguments, 1 );
+    /**
+    * Remove a solicitação de aviso de uma funcao
+    * ----
+    * @recebe: ref - Referencia passada no aviso
+    * @retorna: o próprio
+    * @efeito: remove elementos internos
+    */
+    public esquecaMe ( ref:string ):this
+    {
+        let acao, chave;
 
-                // Chamando a função com os argumentos passados
-                this.solicitacoes[ acao ][ chave ].apply( this, args );
+        if( !this.referencias[ ref ] ) return this;
 
-                // Verificando se só deseja uma unica chamada
-                let i = this.unicos[ acao ].indexOf( chave );
-                if( i >= 0 )
-                {
-                    // Removendo função para evitar ser
-                    // chamada novamente
-                    delete this.solicitacoes[ acao ][ chave ];
-                    delete this.unicos[ i ];
-                }
-            }
+        acao  = this.referencias[ ref ].acao;
+        chave = this.referencias[ ref ].chave;
 
-            return this;
-        }
-
-        /**
-        * Remove a solicitação de aviso de uma funcao
-        * ----
-        * @recebe: ref - Referencia passada no aviso
-        * @retorna: o próprio
-        * @efeito: remove elementos internos
-        */
-        public esquecaMe ( ref:string ):this
-        {
-            let acao, chave;
-
-            if( !this.referencias[ ref ] ) return this;
-
-            acao  = this.referencias[ ref ].acao;
-            chave = this.referencias[ ref ].chave;
-
-            delete this.solicitacoes[ acao ][ chave ];
-            delete this.referencias[ ref ];
-        }
+        delete this.solicitacoes[ acao ][ chave ];
+        delete this.referencias[ ref ];
     }
 }
+
+export { Hermes }
